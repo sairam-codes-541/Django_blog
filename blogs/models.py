85cd37e14391156f6362a9ba26fa2ae0b91bea0d
Sairam <saiyadav79894@gmail.com>
2026-07-07 17:41:24 +0530

    blog app and blog model

---
+++ blogs/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+       return self.category_name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
+STATUS_CHOICES = (
+    ("Draft","Draft"),
+    ("published","Published")
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug =models.SlugField(max_length=150, unique=True, blank=True )
+    category =models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    short_description =models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=2000)
+    featured_image =models.ImageField(upload_to='uploads/%Y/%m/%d')
+    status =models.CharField(choices=STATUS_CHOICES, default="Draft")
+    is_featured =models.BooleanField(default=False)
+    created_at =models.DateTimeField(auto_now_add=True)
+    upadted_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+       return self.title
