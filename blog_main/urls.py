3b9f4dd19a648472d7b533897f07321a83df34cf
Sairam <saiyadav79894@gmail.com>
2026-07-09 14:10:43 +0530

    category crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -26,8 +26,13 @@ urlpatterns = [
     path('', views.home,name="home"),
     path('category/', include('blogs.urls')),
     path('/<slug:slug>/', Blogsview.blogs, name="blogs" ),
+
+
     path('blogs/search/', Blogsview.search, name="search"),
     path('register/', views.register, name='register'),
     path('login/', views.login,name="login"),
     path('logout', views.logout, name="logout"),
+
+    #dashnoards
+    path('dashboard/', include('dashboards.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
