a29a4854ae83e39f7e6415bad394032c0c0cb1f8
Sairam <saiyadav79894@gmail.com>
2026-07-10 12:24:41 +0530

    added comments functionaloty and  only login user can write comment

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Category,Blog
+from django.http import HttpResponseRedirect
+
+from .models import Category,Blog,Comment
 from django.db.models import Q
 
 
@@ -16,10 +18,22 @@ def post_by_category(request,category_id):
 
 def blogs(request, slug):
     single_blog =  get_object_or_404(Blog, slug=slug, status='Published',)
+    #comment
+    if request.method == 'POST':
+        comment = Comment()
+        comment.user = request.user
+        comment.blog = single_blog
+        comment.comment = request.POST['comment']
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    
+    comments = Comment.objects.filter(blog=single_blog)
 
     context = {
         
         "single_blog":single_blog,
+        "comments":comments,
     }
     return render(request, 'blogs.html',context)
 
